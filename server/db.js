e8294eea5df16a750131ba588df3afb80bb10694
PRABHUDAS AVANIGADDA <91600263+Prabhudas111@users.noreply.github.com>
2023-07-12 16:59:03 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");                              
-const mongoURI = "mongodb://127.0.0.1:27017/iNoteBook";                              
+const mongoose = require("mongoose");
+const mongoURI = "mongodb+srv://prabhaaa:prabhaaa9866@cluster0.l9ik2gn.mongodb.net/iNotebook?retryWrites=true&w=majority";
 
-const connectToMongo = () => {                              
+const connectToMongo = () => {
   mongoose
-    .connect(mongoURI, {                              
-      useNewUrlParser: true,                              
-      useUnifiedTopology: true,                              
-    })                              
-    .then(() => {                              
-      console.log("Connected to MongoDB successfully");                              
+    .connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .catch((error) => {                              
-      console.error("Error connecting to MongoDB:", error.message);                              
-    });                              
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error.message);
+    });
 };
 
 module.exports = connectToMongo;
